f721333215105c9d957b121d27ff4d9dc25b3632
Queensferry <queensferry.me@gmail.com>
2021-06-22 00:00:29 +0800

    implement basic plugin system
    https://packaging.python.org/guides/creating-and-discovering-plugins/#using-package-metadata

--- rsserpent/main.py
+++ rsserpent/main.py
@@ -1,8 +1,9 @@
+from importlib_metadata import entry_points
+
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
-
-@app.get("/")
-def index() -> str:
-    return "Hello World!"
+for plugin in entry_points(group="rsserpent.plugins"):
+    app.include_router(plugin.load())
