621bccc7f62b933a4b55d1075836fe29df812a07
Queensferry <queensferry.me@gmail.com>
2021-06-26 21:10:09 +0800

    fix mypy error in pre-commit environment

--- rsserpent/main.py
+++ rsserpent/main.py
@@ -1,24 +1,21 @@
 from functools import partial
 from pathlib import Path
-from typing import cast
+from typing import Optional, cast
 
 import arrow
 from fastapi import Depends, FastAPI, Request, Response
+from fastapi.routing import APIRoute
 from importlib_metadata import entry_points
 from jinja2 import Environment, FileSystemLoader
 
 from .model import Feed, Plugin
 
 
-app = FastAPI()
 templates = Environment(loader=FileSystemLoader(Path(__file__).parent / "templates"))
 templates.globals["arrow"] = arrow
 
 
-# NOTE: I don't know why, but `mypy` refuses to follow imports, resulting
-# in that `Request` & `Response` effectively becomes `Any` type. Temporarily
-# ignores the next line when performing type checks :(
-def endpoint(data: dict, plugin: Plugin, request: Request) -> Response:  # type: ignore
+def endpoint(data: dict, plugin: Plugin, request: Request) -> Response:
     """Define a general endpoint for registering plugins."""
     content = templates.get_template("rss.xml.jinja").render(
         data=Feed(**data), plugin=plugin, request=request
@@ -26,11 +23,16 @@ def endpoint(data: dict, plugin: Plugin, request: Request) -> Response:  # type:
     return Response(content=content, media_type="application/xml")
 
 
+routes = []
 for entry_point in entry_points(group="rsserpent.plugins"):
     plugin = cast(Plugin, entry_point.load())
     for path, provider in plugin.routers.items():
-        app.add_api_route(
-            path,
-            partial(endpoint, data=Depends(provider), plugin=plugin),
-            methods=["GET"],
+        routes.append(
+            APIRoute(
+                path,
+                partial(endpoint, data=Depends(provider), plugin=plugin),
+                methods=["GET"],
+            )
         )
+
+app = FastAPI(routes=cast(Optional[list], routes))
