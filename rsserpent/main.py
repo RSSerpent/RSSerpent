085048a8561c2976cd551d28c17dfbeabdaee486
Creedowl <creedowl@gmail.com>
2021-07-02 21:36:32 +0800

    improve openapi docs (#6)
    * improve openapi docs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- rsserpent/main.py
+++ rsserpent/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import cast
 
 import arrow
-from fastapi import Depends, FastAPI, Request, Response
+from fastapi import APIRouter, Depends, FastAPI, Request, Response
 from importlib_metadata import entry_points
 from jinja2 import Environment, FileSystemLoader
 
@@ -16,12 +16,15 @@ templates.globals["arrow"] = arrow
 
 for entry_point in entry_points(group="rsserpent.plugins"):
     plugin = cast(Plugin, entry_point.load())
+    router = APIRouter()
     for path, provider in plugin.routers.items():
 
-        @app.get(path)
+        @router.get(path, summary=provider.__name__, description=provider.__doc__)
         def endpoint(request: Request, data: dict = Depends(provider)) -> Response:
             """Define a general endpoint for registering plugins."""
             content = templates.get_template("rss.xml.jinja").render(
                 data=Feed(**data), plugin=plugin, request=request
             )
             return Response(content=content, media_type="application/xml")
+
+    app.include_router(router, tags=[plugin.name])
