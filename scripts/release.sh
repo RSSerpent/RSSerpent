043fa5151e8631e8a5742fd0c9cab3761f5c23a4
Queensferry <queensferry.me@gmail.com>
2021-07-06 11:12:28 +0800

    release 0.1.1

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+version=$(poetry version --short)
+
+printf "The current release version is $version.\n"
+printf "Press [y/n] to proceed: "
+
+read yn
+case $yn in
+    [Yy]* )
+        git add --all
+        git commit -v
+        git tag $version;
+        git push --all
+        git push --tags;;
+    * ) exit;;
+esac
