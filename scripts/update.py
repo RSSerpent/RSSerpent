1d180cb4a5221cdeb93fcfe0b367de2362cde380
Queensferry <queensferry.me@gmail.com>
2021-08-26 23:00:37 +0800

    chore: add script to update deps

---
+++ scripts/update.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+import httpx
+
+
+# Update `.pre-commit-config.yaml`
+
+print("Updating .pre-commit-config.yaml")  # noqa: T001
+
+
+def get_package_latest_version(package_name: str) -> str:
+    """Get the latest stable version of a PyPI package."""
+    response = httpx.get(f"https://pypi.org/pypi/{package_name}/json")
+    latest_version: str = response.json()["info"]["version"]
+    return latest_version
+
+
+with open(".pre-commit-config.yaml") as file:
+    text = file.read()
+
+for match in re.findall(r"[\w-]+==[\d\w.]+", text):
+    package_name, current_version = match.split("==")
+    latest_version = get_package_latest_version(package_name)
+    text = re.sub(
+        f"{package_name}=={current_version}", f"{package_name}=={latest_version}", text
+    )
+
+with open(".pre-commit-config.yaml", "w") as file:
+    file.write(text)
+
+
+# Update poetry dependencies
+os.system("poetry update")
+os.system("poetry show -lo")
