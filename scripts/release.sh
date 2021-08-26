0dc6fd2d07395adb8568415d3bb2e7f0ad4c059e
Queensferry <queensferry.me@gmail.com>
2021-08-26 21:32:29 +0800

    chore: update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+poetry version $1
 version=$(poetry version --short)
+
+git diff HEAD
 
 printf "The current release version is $version.\n"
 printf "Press [y/n] to proceed: "
-
 read yn
 case $yn in
     [Yy]* )
         git add --all
         git commit -v
-        git tag $version
-        git push --tags origin master;;
+        git tag $version;;
     * ) exit;;
 esac
