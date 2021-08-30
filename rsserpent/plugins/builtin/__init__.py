cfffae02366b7a9dcc7861e3882c0f5490ca890c
Queensferry <queensferry.me@gmail.com>
2021-08-30 18:49:03 +0800

    test: add integration test for `@cached`

--- rsserpent/plugins/builtin/__init__.py
+++ rsserpent/plugins/builtin/__init__.py
@@ -1,5 +1,5 @@
 from ...models import Persona, Plugin
-from . import example, example_with_args
+from . import example, example_cached, example_with_args
 
 
 plugin = Plugin(
@@ -13,6 +13,7 @@ plugin = Plugin(
     prefix="/_",
     routers={
         example.path: example.provider,
+        example_cached.path: example_cached.provider,
         example_with_args.path: example_with_args.provider,
     },
 )
--- tests/plugins/test_builtin.py
+++ tests/plugins/test_builtin.py
@@ -2,6 +2,8 @@ from hypothesis import given, settings
 from hypothesis.strategies import integers
 from starlette.testclient import TestClient
 
+from rsserpent.plugins.builtin import example_cached
+from rsserpent.utils.cache import get_cache
 from tests.conftest import Times
 
 
@@ -11,6 +13,21 @@ def test_example(client: TestClient) -> None:
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/xml"
     assert response.text.count("<item>") == 1
+
+
+def test_example_cached(client: TestClient) -> None:
+    """Test the `/_example/cached` route."""
+    response1 = client.get("/_/example/cached")
+    response2 = client.get("/_/example/cached")
+    assert response1.text == response2.text
+    assert "<title>Example 1</title>" in response1.text
+
+    cache = get_cache(example_cached.provider)
+    if cache is not None:
+        cache.clear()
+    response3 = client.get("/_/example/cached")
+    assert response1.text != response3.text
+    assert "<title>Example 2</title>" in response3.text
 
 
 @settings(max_examples=Times.SOME)
