06533cec8b6eb286658b5b50049d8ed3b5bf7c95
Queensferry <queensferry.me@gmail.com>
2021-09-07 14:43:13 +0800

    chore: drop proxy support (45f83ad)
    Maybe we will come back to this in the future.

--- rsserpent/models/__init__.py
+++ rsserpent/models/__init__.py
@@ -1,6 +1,5 @@
 from .plugin import Persona, Plugin, ProviderFn
-from .proxy import Proxy
 from .rss import Feed
 
 
-__all__ = ("Feed", "Persona", "Plugin", "ProviderFn", "Proxy")
+__all__ = ("Feed", "Persona", "Plugin", "ProviderFn")
