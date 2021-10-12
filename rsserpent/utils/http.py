b19c2181789de81fd919a3602a4fa275102dc43a
Queensferry <queensferry.me@gmail.com>
2021-10-12 17:43:10 +0800

    build: bump dev deps

--- rsserpent/utils/http.py
+++ rsserpent/utils/http.py
@@ -6,7 +6,10 @@ from pyppeteer import launch
 from pyppeteer.page import Page
 
 
-ua = UserAgent()
+ua = UserAgent(
+    fallback="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+)
 
 
 class Browser:
